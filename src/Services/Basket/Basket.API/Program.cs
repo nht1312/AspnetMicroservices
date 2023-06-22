770d0920ea9d17782fd6ff9e05cecf816b1b204c
itnhtu <itnhtu@gmail.com>
2023-06-22 13:43:33 +0700

    container management with portainer & completed basket services

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connect redis
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     var configuration = builder.Configuration.GetSection("CacheSettings");
     options.Configuration = configuration.GetValue<string>("ConnectionString");
 });
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 var app = builder.Build();
 
