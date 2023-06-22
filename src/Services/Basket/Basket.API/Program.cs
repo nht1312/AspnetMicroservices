5c673ffa1652e4a460c96617e7997fb85c3f0cf1
itnhtu <itnhtu@gmail.com>
2023-06-22 10:24:16 +0700

    Connect redis docker container & developing repository pattern

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    var configuration = builder.Configuration.GetSection("CacheSettings");
+    options.Configuration = configuration.GetValue<string>("ConnectionString");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
