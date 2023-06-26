5a1bc0a20f96522c05676af1aea4f6277059f199
itnhtu <itnhtu@gmail.com>
2023-06-26 09:18:00 +0700

    containerize, add postgresql image into docker-compose, migrated postgresql db when microservices startup

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IDiscountRepository , IDiscountRepository>();
+builder.Services.AddScoped<IDiscountRepository , DiscountRepository>();
 var app = builder.Build();
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
